d91844313f02457b41a3209e27bb7e62a59fe9fa
Leanne <leanney98@gmail.com>
2020-10-11 22:40:00 -0600

    changing js for header colours

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ if ((width >= 1195)){
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
         $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
+            $(this).css("border-bottom","2px solid rgb(56, 71, 114)");
         },function(){
             $(this).css("border-bottom","2px solid transparent");
         });
     }else{
         $("#header").css("background","transparent");
-        $("#header").css("color","#fff");
+        $("#header").css("color","#000");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
         $("#header").css("padding","6vh 4vw");
         $("#navigation a").hover(function(){
